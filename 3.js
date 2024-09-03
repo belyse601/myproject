7081025bbc568f18e11e8a433149eff310e45307
belyse601 <irkzbelyse6@gmail.com>
2024-09-03 04:08:51 -0700

    Add files via upload
    yyy

---
+++ 3.js
@@ -0,0 +1,6 @@
+function product() {
+    let a=document.querySelector('#n1').value
+     let b=document.querySelector('#n2').value
+     let  s=Number(a)*Number(b)
+     document.querySelector('#result').innerHTML=product
+ 
